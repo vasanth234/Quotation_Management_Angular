3b25d5f72681cbba92b18e0a2090f1dd323751ce
vasanth234 <pipparivasanth66700@gmail.com>
2025-03-23 00:48:47 +0530

    commiting new changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { CreateRFQComponent } from './pages/create-rfq/create-rfq.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:"",
+        redirectTo:"home",
+        pathMatch:"full"
+    },
+    {
+        path:'home',
+        component:LandingComponent
+    },
+    {
+        path:'register',
+        component:RegistrationComponent
+    },
+    {
+        path:'create-rfq',
+        component:CreateRFQComponent
+    }
+];
