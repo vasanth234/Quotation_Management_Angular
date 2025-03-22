5af08bbed7f463522d0ec852ac0e82ecbbf0e79b
vasanth234 <pipparivasanth66700@gmail.com>
2025-03-23 00:54:29 +0530

    added few chnages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 import { CreateRFQComponent } from './pages/create-rfq/create-rfq.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
 
@@ -17,6 +18,10 @@ export const routes: Routes = [
     {
         path:'register',
         component:RegistrationComponent
+    },
+    {
+        path:'login',
+        component:LoginComponent
     },
     {
         path:'create-rfq',
